3cf374733cb44218953c662b7bb531a927d544d0
conn01sseur <x1nder@proton.me>
2025-05-09 04:35:08 +0900

    Update wintool.py

--- wintool.py
+++ wintool.py
@@ -113,7 +113,7 @@ def main():
     print('''    [[blue]2[/blue]] - [yellow]Optimization windows[/yellow]  | [bright_black]Отключение ненужных служб[/bright_black]''')
     print('''    [[blue]3[/blue]] - [yellow]Activation windows[/yellow]    | [bright_black]Активация Windows[/bright_black]''')
     print()
-    print('''    [red]GitHub[/red] --> https://github.com/conn01sseur/''')
+    print('''    [red]Code by[/red] [purple]conn01sseur[/purple]''')
     print()
 
     a = input("  --> ")
